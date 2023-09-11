bc5fbfccd1c669ef4780bfa7a30364283008bd5f
dsl2022 <2015rpro@gmail.com>
2023-09-11 19:30:05 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const board = [
+    ["", "", ""], 
+    ["", "", ""],
+    ["", "", ""] 
+]; 
+
+
+const test =document.querySelectorAll('.cell');
+test[0].addEventListener('click',function(){
+    test[0].textContent = "X"
+})
